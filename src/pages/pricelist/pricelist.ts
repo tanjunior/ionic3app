80c8cc39778e4360a8a263d86f0bd6056a5ccd0e
Spastic <junior.tan@live.com>
2018-02-20 03:50:59 +0800

    retrieve pricelist from database

--- src/pages/pricelist/pricelist.ts
+++ src/pages/pricelist/pricelist.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
+//import firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -10,114 +11,27 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class PricelistPage {
   profileKey: string = this.navParams.get('uid');;
   day: string = "thursday";
-  pricelist2: any;
-  pricelist: any = {
-    "monday": {
-      "All Night": {
-        "0": {
-          "name": "1 Martell",
-          "price": "219"
-        },
-        "1": {
-          "name": "3 Martell",
-          "price": "539"
-        },
-        "2": {
-          "name": "5 Martell (Free 2 tower)",
-          "price": "999"
-        }
-      }
-    },
-    "tuesday": {
-      "All Night": {
-        "0": {
-          "name": "1 Martell",
-          "price": "219"
-        },
-        "1": {
-          "name": "3 Martell",
-          "price": "539"
-        },
-        "2": {
-          "name": "5 Martell (Free 2 tower)",
-          "price": "999"
-        }
-      }
-    },
-    "wednesday": {
-      "All Night": {
-        "0": {
-          "name": "1 Martell",
-          "price": "219"
-        },
-        "1": {
-          "name": "3 Martell",
-          "price": "539"
-        },
-        "2": {
-          "name": "5 Martell (Free 2 tower)",
-          "price": "999"
-        }
-      }
-    },
-    "thursday": {
-      "9PM - 9:30PM": {
-        "0": {
-          "name": "1 tower",
-          "price": "39"
-        },
-        "1": {
-          "name": "2 tower",
-          "price": "69"
-        }
-      },
-      "9:30PM - 10:30PM": {
-        "0": {
-          "name": "1 tower",
-          "price": "59"
-        },
-        "1": {
-          "name": "2 tower",
-          "price": "99"
-        }
-      },
-      "After 10:30PM": {
-        "0": {
-          "name": "1 tower",
-          "price": "99"
-        },
-        "1": {
-          "name": "2 tower",
-          "price": "149"
-        },
-        "2": {
-          "name": "3 tower",
-          "price": "199"
-        }
-      },
-      "All Night": {
-        "0": {
-          "name": "1 Martell",
-          "price": "219"
-        },
-        "1": {
-          "name": "3 Martell",
-          "price": "539"
-        },
-        "2": {
-          "name": "5 Martell (Free 2 tower)",
-          "price": "999"
-        }
-      }
-    }
-  }
+  pricelist: Array<any> = [];
+  //itemRef: firebase.database.Reference = firebase.database().ref(`/pricelists/${this.profileKey}`);
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase) {
+
+  }
+
+  ionViewWillLoad() {
     this.db.object(`/pricelists/${this.profileKey}/pricelist`).snapshotChanges().subscribe(data => {
-      this.pricelist2 = data.payload.val();
-      console.log(this.pricelist2);
+      this.pricelist = data.payload.val();
+      console.log(this.pricelist);
     });
 
+    /*  not sure why this is not working */
+    /*  this.itemRef.on('value', itemSnapshot => {
+          this.pricelist = [];
+          itemSnapshot.forEach(itemSnap => {
+            this.pricelist.push(itemSnap.val());
+            return false;
+          });
+        }); */
   }
 
   getTimings(day: any) {
